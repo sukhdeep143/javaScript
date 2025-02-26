b484e9559a9919b3a5590786ad0db980bdde1a20
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-02-26 14:31:45 +0530

    DOM is completed

--- JavaScript execute in HTML/script.js
+++ JavaScript execute in HTML/script.js
@@ -41,6 +41,50 @@ document.getElementById("removeLastTask").addEventListener("click", function(){
    taskList.lastChild.remove();
 })
 
+// Example 6 
+
+document.getElementById("ClickMeToKnowEventHandling").addEventListener("click", function(){
+   alert("On Click")
+})
+
+document.getElementById("MouseHover").addEventListener("mouseover", function(){
+   alert("You have hover over the button")
+})
+
+document.getElementById("Dbclick").addEventListener("dblclick", function(){
+   alert("You clicked two time to work with this button")
+})
 
 
+// Example 7
 
+document.getElementById("tealist").addEventListener("click", function(event){
+   if(event.target && event.target.matches(".teaItem")){
+      alert(`you have Selected ${event.target.textContent}`)
+   }
+})
+
+// Example 8 
+
+document.getElementById("feedbackForm").addEventListener("submit", function(event){
+   event.preventDefault();
+   let feedback = document.getElementById("FeedbackInput").value;
+   console.log(feedback);
+   document.getElementById("FeedbackDisplay").textContent = `feedback for get the value: ${feedback}`
+})
+
+// Example 9 
+
+document.addEventListener("DOMContentLoaded", function(){
+   document.getElementById("domStatus").textContent = "DOM Content id fully Loaded!!"
+})
+
+// Example 10
+
+document.getElementById("toggleHighlight").addEventListener("click", function(){
+   let colorAddToText = document.getElementById("descriptionText");
+   colorAddToText.classList.toggle("highlight")
+   colorAddToText.style.color = "red";
+
+
+})
