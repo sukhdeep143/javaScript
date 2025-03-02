9e9111adb9f897ab9687512ef47305076750ba1b
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-03-02 13:21:13 +0530

    promise structure

--- AdvanceJs/Promises.js
+++ AdvanceJs/Promises.js
@@ -1,3 +1,32 @@
 /*
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
 */
+
+function fetchData(){
+    return new Promise((keep_the_promise, reject_the_promise)=>{
+        setTimeout(()=>{
+            let success = false;
+            if(success){
+                keep_the_promise("The promise we made are not broken yet");
+            } else{
+                reject_the_promise("All the promise we made are broken!!!");
+            }
+
+        }, 2000);
+    })
+    
+}
+
+
+fetchData()
+    .then((data)=>{
+        console.log(data);
+        
+        
+
+    })
+    .catch((error)=>{
+        let who_end = "It was you!!!";
+        console.log(error, who_end);
+        
+    })
